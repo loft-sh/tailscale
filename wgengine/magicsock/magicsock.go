ed0f69479c4bd12ee0ec749a29a70ebf6d9109b9
Fabian Kramm <fab.kramm@googlemail.com>
2025-02-22 12:10:28 +0100

    fix: do not periodicaly rebind

--- wgengine/magicsock/magicsock.go
+++ wgengine/magicsock/magicsock.go
@@ -888,15 +888,6 @@ func (c *Conn) updateEndpoints(why string) {
 		c.muCond.Broadcast()
 	}()
 	c.dlogf("[v1] magicsock: starting endpoint update (%s)", why)
-	if c.noV4Send.Load() && runtime.GOOS != "js" && !c.onlyTCP443.Load() && !hostinfo.IsInVM86() {
-		c.mu.Lock()
-		closed := c.closed
-		c.mu.Unlock()
-		if !closed {
-			c.logf("magicsock: last netcheck reported send error. Rebinding.")
-			c.Rebind()
-		}
-	}
 
 	endpoints, err := c.determineEndpoints(c.connCtx)
 	if err != nil {
